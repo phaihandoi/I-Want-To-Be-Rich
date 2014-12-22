62ac1539e510b3a9c6527f98a23d173220be5000
Phan Hoang Duy <Phanduyna@gmail.com>
2014-12-22 23:56:17 +0700

    first commit

---
+++ millionaire/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  millionaire
+//
+//  Created by H*D on 12/16/14.
+//  Copyright (c) 2014 duyPhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "millionaireAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([millionaireAppDelegate class]));
+    }
+}
---
+++ millionaire/millionaireViewController.h
@@ -0,0 +1,16 @@
+//
+//  millionaireViewController.h
+//  millionaire
+//
+//  Created by H*D on 12/16/14.
+//  Copyright (c) 2014 duyPhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface millionaireViewController : UIViewController{
+    int timeDown;
+    NSTimer *repeatTime;
+}
+
+@end
